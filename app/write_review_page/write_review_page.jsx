d4a54ab53eabeec6b0c4e46da1560dcf9d510bd7
Kim Sereyvath <kimsereyvath20@kit.edu.kh>
2023-10-03 17:51:16 +0700

    make first page of write review become responsive

--- app/write_review_page/write_review_page.jsx
+++ app/write_review_page/write_review_page.jsx
@@ -6,16 +6,16 @@ export default function WriteReviewPage() {
     items.push([i, "Ai Software", i]);
   }
   return (
-    <div className="w-full p-9 font-dmsan">
-      <div className="px-5 pt-8 rounded-2xl bg-[#0B0D14] text-white">
-        <div className="grid grid-cols-2">
-          <div className=" pt-6 ml-24 mr-44 grid place-content-center">
-            <p className="a text-5xl">Using software?</p>
-            <button className="bg-[#2F455C] w-full p-4 rounded-[500px] mt-5 text-2xl">
+    <div className="w-full lg:p-9 p-5 font-dmsan">
+      <div className="lg:px-5 lg-8 rounded-2xl bg-[#0B0D14] text-white">
+        <div className="grid md:grid-cols-2">
+          <div className="pt-6 xl:ml-24 xl:mr-44 lg:ml-12 grid place-content-center">
+            <p className="lg:text-5xl text-4xl">Using software?</p>
+            <button className="bg-[#2F455C] w-full lg:p-4 p-3 rounded-[500px] mt-5 lg:text-2xl text-xl">
               Write a review
             </button>
           </div>
-          <div className="mt-5 mx-24">
+          <div className="mt-5 md:mt-14 lg:mx-24 mx-10">
             <div className="text-black px-3 py-1 bg-[#D9D9D9] rounded-full flex">
               <div className="pr-1 pt-1">
                 <iconify-icon icon="bi:search"></iconify-icon>
@@ -28,68 +28,87 @@ export default function WriteReviewPage() {
                 className="outline-none bg-transparent w-full ml-2"
               />
             </div>
-            <p className="text-2xl mt-6">
+            <p className="lg:text-2xl text-xl mt-6 text-center">
               Help million of people make the right choice for their business.
             </p>
           </div>
         </div>
-        <div className="w-full py-16 px-20">
-          <img src="write_review/write_review_page_process_bar.png" alt="" />
+        <div className="w-full lg:py-16 lg:px-20 sm:px-32 py-10 px-8">
+          <img
+            src="write_review/write_review_page_process_bar.png"
+            alt=""
+            className="lg:block hidden"
+          />
+          <img
+            src="write_review/write_review_page_process_bar_mobile.png"
+            alt=""
+            className="lg:hidden block"
+          />
         </div>
       </div>
       <div className="flex justify-between my-5">
-        <div className="flex place-items-center">
-          <p>Filter by</p>
-          <button className="bg-[#E3E6EA] ml-3 py-2 px-4 rounded-xl">Category</button>
+        <div className="md:flex place-items-center">
+          <p className="text-center">Filter by</p>
+          <select className="outline-none bg-[#E3E6EA] ml-3 py-2 px-4 rounded-xl">
+            {[1, 2, 3].map((item, index) => {
+              return (
+                <option value="category" key={index}>
+                  Category {item}
+                </option>
+              );
+            })}
+          </select>
         </div>
-        <div className="flex place-items-center">
-          <p>Sort by</p>
-          <button className="bg-[#E3E6EA] ml-3 py-2 px-4 rounded-xl">Popularity</button>
+        <div className="md:flex place-items-center">
+          <p className="text-center">Sort by</p>
+          <select className="outline-none bg-[#E3E6EA] ml-3 py-2 px-4 rounded-xl">
+            {[1, 2, 3].map((item, index) => {
+              return (
+                <option value="popularity" key={index}>
+                  Popularity {item}
+                </option>
+              );
+            })}
+          </select>
         </div>
       </div>
-      <div className="grid sm:grid-cols-3 md:grid-cols-5 grid-cols-2">
+      <div className="grid md:grid-cols-3 xl:grid-cols-5 grid-cols-2">
         {items.map((item) => {
           return Item(item[0], item[1], item[2]);
         })}
       </div>
       <div className="grid place-items-center w-full">
-        <div className="flex mt-3">
+        <div className="flex place-items-center mt-3">
           {[1, 2, 3, 4, 5, 6].map((item, index) => {
             return (
               <button
                 key={index}
-                className="bg-[#E3E6EA] mx-1 p-2 w-12 h-12 rounded-full"
+                className="bg-[#E3E6EA] mx-1 p-2 md:w-12 md:h-12 w-9 h-9 rounded-full"
               >
                 {item}
               </button>
             );
           })}
-          <button className="bg-[#E3E6EA] mx-1 p-2 w-12 h-12 rounded-full grid place-content-center">
+          <button className="bg-[#E3E6EA] mx-1 p-2 md:w-12 md:h-12 w-10 h-10 rounded-full grid place-content-center">
             <img src="write_review\icons\next.png" alt="" />
           </button>
         </div>
       </div>
-      {/* <div className="stack"></div> */}
-      <div className="w-full mt-20 grid place-content-center place-items-center">
-        <div className="absolute z-10 px-8 py-20 w-3/5 left-1/2 -translate-x-[63%]">
-          <p className="relative text-[3.5rem] font-bold">
+      <div className="w-full xl:mt-20 mt-16 lg:px-28 md:px-20 sm:px-16">
+        <div className="relative w-full rounded-3xl bg-[url('../public/write_review/vendor_background.png')] bg-cover bg-center">
+          <p className="relative xl:text-5.5xl lg:text-4xl md:text-3xl text-xl xl:ml-12 ml-7 xl:pt-16 pt-8 font-bold xl:w-[75%]">
             Start and make a different in your company.
           </p>
-          <p className="relative w-[75%] text-2xl mt-5 text-[#2F455C]">
+          <p className="relative xl:w-[55%] w-[55%] xl:text-2xl md:text-xl text-sm xl:mt-6 mt-2 xl:ml-12 ml-7 text-[#2F455C]">
             Not being able to convey the goodness of our products because people
             think it's just a sales pitch after all.{" "}
           </p>
-          <button className="bg-[#2F455C] relative mt-20 py-5 px-14 text-2xl font-semibold text-white rounded-full">
+          <button className="bg-[#2F455C] relative xl:ml-12 ml-5 xl:mt-20 mt-5 xl:mb-11 mb-7 xl:py-5 md:py-3 xl:px-14 md:px-7 xl:text-2xl md:text-xl text-sm px-4 py-2 font-semibold text-white rounded-full">
             Post your service for free
           </button>
         </div>
-          <img
-            src="write_review\vendor_background.png"
-            alt=""
-            className="relative z-0 w-[93%]"
-          />
-        </div>
-      <div className="h-16"></div>
+      </div>
+      <div className="h-96"></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
+      fontSize: {
+        "5.5xl": [
+          "3.5rem",
+          {
+            lineHeight: "1.2",
+            letterSpacing: '0.04rem',
+          },
+        ],
       },
     },
     fontFamily: {
