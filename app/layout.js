68cf2cb9b93be91df39a331ce42610bcb4ead082
Thea-zin <bansothea20@kit.edu.kh>
2023-10-18 23:57:25 +0700

    change name file

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
-import Navbar from "./components/navbar";
+import Navbar from "./Components/navbar";
 
-import Footer from "./components/footer";
+import Footer from "./Components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
