4154a68162d5fb7101c623e839e1084485782256
Thea-zin <bansothea20@kit.edu.kh>
2023-12-25 16:29:10 +0700

    fix profile click

--- app/Components/navbar.jsx
+++ app/Components/navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
   const auth=getAuth(firebase_app)
 
     useEffect(() => {
-      //signOut(auth);
+      signOut(auth);
       const user = auth.currentUser;
       setIsUser(user !== null && user.accessToken !== null);
       
@@ -61,10 +61,26 @@ const Navbar = () => {
             </div>
         
              <div className=' flex gap-5 justify-center items-center' >
-                <button onClick={openPopUpSignIn}>Log in</button>
-                <button onClick={openPopUpSignUp} className='bg-darkblue p-2 text-white text-center rounded-full w-24 hover:bg-sky-700 cursor-pointer'>Sign Up</button>
+                {isUser ? (
+                // Render these elements if isUser is true
+                    <p><Link href='/pages/mainCategories'><div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                    <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                </div></Link></p>
+               ) : (
+                // Render this element if isUser is false
+             <>
+                  <button onClick={openPopUpSignIn}>Log in</button>
+                  <button
+                    onClick={openPopUpSignUp}
+                    className='bg-darkblue p-2 text-white text-center rounded-full w-24 hover:bg-sky-700 cursor-pointer'
+                  >
+                    Sign Up
+                  </button>
+                </>
+              )}
             </div>
-            {isUser ?(<p>User</p>) : (<p>Is not a user</p>)}
+            
+
            
             
         </div>
