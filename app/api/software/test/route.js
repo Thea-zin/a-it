e2632a7fca0cdd23c9cb1e57fe663f0a6ab3dcbb
Kim Sereyvath <kimsereyvath20@kit.edu.kh>
2024-04-05 22:21:22 +0700

    testing web scraping under production constraint.

--- app/api/software/test/route.js
+++ app/api/software/test/route.js
@@ -29,103 +29,39 @@ import {
   updateProfile,
   connectAuthEmulator,
 } from "firebase/auth";
-import {
-  ApiClient,
-  BodyPart,
-  EmailMessageData,
-  EmailRecipient,
-  EmailsApi,
-} from "@elasticemail/elasticemail-client";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import puppeteer from "puppeteer";
+
+const auth = getAuth(firebase_app);
+const firestore = getFirestore(firebase_app);
 
 // export async function POST(req) {
 //   try {
-//     const firestore = getFirestore(firebase_app);
 //     const request = await req.json();
-//     // let q = query(
-//     //   collection(firestore, "softwares"),
-//     //   orderBy("nci"),
-//     // );
+//     try {
+//       const userCredential = await createUserWithEmailAndPassword(
+//         auth,
+//         request.email,
+//         request.password
+//       );
+//       const user = userCredential.user;
+//       const userProfile = {
+//         displayName: `${request.name}!បំបែក!${request.occupation}`,
+//       };
+//       const temp = await addDoc(collection(firestore, "verifyemail"), {
+//         email: request.email,
+//         uid: user.uid,
+//       });
+//       console.log("firestore data id", temp.id);
 
-//     // const documentSnapshots = await getDocs(q);
-
-//     let softwares = [];
-//     // for (let doc of documentSnapshots.docs) {
-//     //   let temp = doc.data();
-//     //   softwares.push(temp.nci);
-//     // }
-
-//     const docRef = doc(firestore, "categories", "categories");
-//     const docSnap = await getDoc(docRef);
-
-//     let categories = docSnap.data();
-//     categories.categories.sort()
-
-//     const temp = await setDoc(docRef, categories)
-
-//     return NextResponse.json({ softwares: softwares }, { status: 200 });
-//   } catch (e) {
-//     console.log(e);
-//     return NextResponse.json(
-//       { message: "something wrong on the server side!" },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-// export async function POST(req) {
-//   try {
-//     let defaultClient = ApiClient.instance;
-
-//     let apikey = defaultClient.authentications["apikey"];
-//     apikey.apiKey =
-//       "9221C78870B21BC5DE9FCE0FB7CE9CF42C6D354F55B680A4845632EEF271C7594CE35265398D0EF0D6D83BAF8A7E5E74";
-
-//     let api = new EmailsApi();
-
-//     const emailData = {
-//       Recipients: {
-//         To: ["serey.vathg@gmail.com"],
-//       },
-//       Content: {
-//         Body: [
-//           {
-//             ContentType: "HTML",
-//             Charset: "utf-8",
-//             Content: "Mail content.",
-//           },
-//           {
-//             ContentType: "PlainText",
-//             Charset: "utf-8",
-//             Content: "Mail content.",
-//           },
-//         ],
-//         From: "ksvdevtester@gmail.com",
-//         Subject: "Example transactional email",
-//       },
-//     };
-
-//     // let email = EmailMessageData.constructFromObject({
-//     //   Recipients: [new EmailRecipient("MeowWow ")],
-//     //   Content: {
-//     //     Body: [
-//     //       BodyPart.constructFromObject({
-//     //         ContentType: "HTML",
-//     //         Content: "My test email content ;)",
-//     //       }),
-//     //     ],
-//     //     Subject: "JS EE lib test",
-//     //     From: "MyEmail ",
-//     //   },
-//     // });
-
-//     var callback = function (error, data, response) {
-//       if (error) {
-//         console.error(error);
-//       } else {
-//         console.log("API called successfully.");
-//       }
-//     };
-//     api.emailsTransactionalPost(emailData, callback);
+//       await sendEmailVerification(user);
+//       await updateProfile(user, userProfile);
+//     } catch (error) {
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       console.log(errorMessage);
+//       return NextResponse.json({ data: "" }, { status: 405 });
+//     }
 
 //     return NextResponse.json({ data: "" }, { status: 200 });
 //   } catch (e) {
@@ -137,42 +73,104 @@ import {
 //   }
 // }
 
-// const auth = getAuth();
-// connectAuthEmulator(auth, "http://127.0.0.1:9099");
-// const firestore = getFirestore();
-// connectFirestoreEmulator(firestore, "127.0.0.1", 8080);
+const getCategoriesLink = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+  const page = await browser.newPage();
 
-const auth = getAuth(firebase_app);
-const firestore = getFirestore(firebase_app);
+  await page.goto("https://www.futurepedia.io/ai-tools", {
+    waitUntil: "domcontentloaded",
+  });
+
+  const quotes = await page.evaluate(() => {
+    const quoteList = document.querySelectorAll("a");
+
+    return Array.from(quoteList).map((link) => {
+      const text = link.innerText;
+      const href = link.href;
+      if (text.includes("Show all")) {
+        return href;
+      }
+    });
+  });
+  const categories = quotes.filter((text) => {
+    return text;
+  });
+  await browser.close();
+
+  return categories;
+};
+
+const getSubCategoriesLink = async (lnk) => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+  const page = await browser.newPage();
+
+  await page.goto(lnk, {
+    waitUntil: "domcontentloaded",
+  });
+
+  const quotes = await page.evaluate(() => {
+    const quoteList = document.querySelectorAll("h2.capitalize");
+
+    return Array.from(quoteList).map((a) => {
+      const href = a.querySelector("a").href;
+      return href;
+    });
+  });
+  await browser.close();
+
+  return quotes;
+};
+
+const getSoftwareInfo = async (lnk) => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+  const page = await browser.newPage();
+
+  await page.goto(lnk, {
+    waitUntil: "domcontentloaded",
+  });
+
+  const quotes = await page.evaluate(() => {
+    const quoteList = document.querySelectorAll("a div img");
+
+    return Array.from(quoteList).map((a) => {
+      const src = a.src;
+      return src;
+    });
+  });
+  await browser.close();
+
+  return quotes;
+};
 
 export async function POST(req) {
+  // const storage = getStorage(firebase_app);
+
   try {
-    const request = await req.json();
-    try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        request.email,
-        request.password
-      );
-      const user = userCredential.user;
-      const userProfile = {
-        displayName: `${request.name}!បំបែក!${request.occupation}`,
-      };
-      const temp = await addDoc(collection(firestore, "verifyemail"), {
-        email: request.email,
-        uid: user.uid,
-      });
-      console.log("firestore data id", temp.id);
+    // const categoriesLink = await getCategoriesLink();
+    // categoriesLink.forEach((link) => {});
+    // const subcat = await getSubCategoriesLink(
+    //   "https://www.futurepedia.io/ai-tools/productivity"
+    // );
+    const srcs = await getSoftwareInfo(
+      "https://www.futurepedia.io/ai-tools/personal-assistant"
+    );
+    console.log(srcs);
 
-      await sendEmailVerification(user);
-      await updateProfile(user, userProfile);
-    } catch (error) {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorMessage);
-      return NextResponse.json({ data: "" }, { status: 405 });
-    }
-
+    // const image = await fetch(
+    //   "https://www.futurepedia.io/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fu0v1th4q%2Fproduction%2Fb80bca90c5a3a372ec071cbbb097cf974986336f-1024x1024.jpg%3Ffp-x%3D0.5%26fp-y%3D0.5%26w%3D120%26h%3D120%26sharp%3D100%26q%3D100%26auto%3Dformat%26dpr%3D3&w=128&q=75"
+    // );
+    // const imageBlob = await image.blob();
+    // const imgref = ref(storage, `icons/${"testfromweb"}`);
+    // const tlogo = await uploadBytes(imgref, imageBlob);
     return NextResponse.json({ data: "" }, { status: 200 });
   } catch (e) {
     console.log(e);
